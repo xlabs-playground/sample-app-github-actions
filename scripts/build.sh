ad208f681ad125e32522adc09112aefea9332bcd
Sebastian Scatularo <sebastian.scatularo@xlabs.xyz>
2023-05-19 13:43:40 -0300

    update package.json

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,13 +1,15 @@
 #!/bin/env bash
 source $(dirname -- ${0})/common.sh
 
-DEPLOY=${1}
+ARTIFACT_NAME=${1}
+ENV=${2}
 ARTIFACTS=${PWD}/${DEPLOY}
 
 if [[ -d ${ARTIFACTS} ]]; then
     for BRANCH_SHA in $(ls ${ARTIFACTS}); do
-        VERSION=$(cat ${ARTIFACTS}/${BRANCH_SHA}/.latest)
-        install ${BRANCH_SHA} ${VERSION}
+        VERSION=$(cat ${ENV}/${BRANCH_SHA}/.latest)
+        install ${BRANCH_SHA} ${ARTIFACT_NAME} ${VERSION}
+        echo Installed ${BRANCH_SHA} ${ARTIFACT_NAME} ${VERSION}
     done
     mv node_modules build
 else
--- scripts/common.sh
+++ scripts/common.sh
@@ -3,15 +3,19 @@ package_name() {
 }
 
 unpublish() {
+    local PKG_NAME=$
     local VERSION=${1}
-    local PKG_NAME=$(package_name)
     echo "Unpublishing ${PKG_NAME}@${VERSION}..."
     npm unpublish ${PKG_NAME}@${VERSION} --force --registry https://npm.pkg.github.com
 }
 
+# Alias is the branch name sha
+# Version is the version of the package
+# Package name is the name of the package
+# Example: install 1234567890 my-package 1.0.0
 install() {
-    local PKG_NAME=$(package_name)
     local ALIAS=${1}
-    local VERSION=${2}
-    npm install ${1}@npm:${PKG_NAME}@${VERSION}
+    local PKG_NAME=${2}
+    local VERSION=${3}
+    npm install ${ALIAS}@npm:${PKG_NAME}@${VERSION}
 }
