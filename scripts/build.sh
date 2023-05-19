2998043dcba04af240174452951227f06789f38e
Sebastian Scatularo <sebastian.scatularo@xlabs.xyz>
2023-05-19 13:28:17 -0300

    update check

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,7 +4,7 @@ source $(dirname -- ${0})/common.sh
 DEPLOY=${1}
 ARTIFACTS=${PWD}/${DEPLOY}
 
-if [[ -f ${ARTIFACTS} ]]; then
+if [[ -d ${ARTIFACTS} ]]; then
     for BRANCH_SHA in $(ls ${ARTIFACTS}); do
         VERSION=$(cat ${ARTIFACTS}/${BRANCH_SHA}/.latest)
         install ${BRANCH_SHA} ${VERSION}
