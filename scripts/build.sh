645d5627a9ca364a2e7e1182e974ed0ce773bc96
Sebastian Scatularo <sebastian.scatularo@xlabs.xyz>
2023-05-19 13:52:30 -0300

    fix build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,18 +1,26 @@
 #!/bin/env bash
-set +x
-source $(dirname -- ${0})/common.sh
+ROOT=${1}
+PKG_NAME=${2}
+CLUSTER=${3}
 
-ARTIFACT_NAME=${1}
-ENV=${2}
-ARTIFACTS=$(dirname -- ${0})/${DEPLOY}
+# Alias is the branch name sha
+# Version is the version of the package
+# Package name is the name of the package
+# Example: install 1234567890 my-package 1.0.0
+install() {
+    local ALIAS=${1}
+    local PKG_NAME=${2}
+    local VERSION=${3}
+    npm install ${ALIAS}@npm:${PKG_NAME}@${VERSION}
+}
 
-if [[ -d ${ARTIFACTS} ]]; then
-    for BRANCH_SHA in $(ls ${ARTIFACTS}); do
-        VERSION=$(cat ${ENV}/${BRANCH_SHA}/.latest)
-        install ${BRANCH_SHA} ${ARTIFACT_NAME} ${VERSION}
-        echo Installed ${BRANCH_SHA} ${ARTIFACT_NAME} ${VERSION}
+if [[ -d ${CLUSTER} ]]; then
+    for BRANCH_SHA in $(ls ${CLUSTER}); do
+        VERSION=$(cat ${ROOT}/${CLUSTER}/${BRANCH_SHA}/.latest)
+        install ${BRANCH_SHA} ${PKG_NAME} ${VERSION}
+        echo Installed ${BRANCH_SHA} ${PKG_NAME} ${VERSION}
     done
     mv node_modules build
 else
-    echo "No artifacts found at ${ARTIFACTS}"
+    echo "No artifacts found at ${CLUSTER}"
 fi
--- scripts/common.sh
+++ scripts/common.sh
@@ -1,21 +1,6 @@
-package_name() {
-    echo $(node -p -e 'require("./package.json").name')
-}
-
 unpublish() {
     local PKG_NAME=$
     local VERSION=${1}
     echo "Unpublishing ${PKG_NAME}@${VERSION}..."
     npm unpublish ${PKG_NAME}@${VERSION} --force --registry https://npm.pkg.github.com
-}
-
-# Alias is the branch name sha
-# Version is the version of the package
-# Package name is the name of the package
-# Example: install 1234567890 my-package 1.0.0
-install() {
-    local ALIAS=${1}
-    local PKG_NAME=${2}
-    local VERSION=${3}
-    npm install ${ALIAS}@npm:${PKG_NAME}@${VERSION}
 }
